cb39a068525d0b7070573c4fe6c9a8947127bb74
isaacvallentin <isaac@logcb.com>
2017-02-07 21:27:56 +0100

    js op

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@ require('lib/jquery.min.js');
 require('lib/masonry.pkgd.min.js');
 require('lib/list.min.js');
 require('lib/keyboard-pagination.js');
+
+$('.indexgrid').masonry({
+	itemSelector: '.grid-item',
+	columnWidth: '.grid-sizer',
+	percentPosition: true
+});
